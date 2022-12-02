f6ca865cbf10a5f4222487ee3e4a686817146d56
MarcosSantelices <marcos.santelices@uc.cl>
2022-12-01 23:04:10 -0300

    Demo 2.1

---
+++ stored_procedures/importar_usuarios.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE FUNCTION
+
+--hecho para productoras
+
+-- declaramos la función y sus argumentos
+importar_usuarios (nombre varchar(100), pais varchar(30), inicio_actividades date, contacto bigint)
+
+-- declaramos lo que retorna, en este caso un booleano
+RETURNS BOOLEAN AS $$
+
+-- definimos nuestra función
+BEGIN
+
+    IF nombre NOT IN (SELECT nombre from usuarios) THEN
+        INSERT INTO usuarios values(nombre, nombre, "productora");
+
+        -- retornamos true si se agregó el valor
+        RETURN TRUE;
+    ELSE
+        -- y false si no se agregó
+        RETURN FALSE;
+
+    END IF;
+
+-- finalizamos la definición de la función y declaramos el lenguaje
+END
+$$ language plpgsql
