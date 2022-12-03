9c688be5435a79dd1586eeac973def754b0d633c
emiliochandia <emilio.chandia@uc.cl>
2022-12-02 23:07:16 -0300

    dblink12

--- stored_procedures/crear_usuario_productora.sql
+++ stored_procedures/crear_usuario_productora.sql
@@ -14,7 +14,7 @@ DECLARE
     v_context TEXT;
 BEGIN
     PERFORM dblink_connect('db2', 'dbname=grupo105e3 user=grupo105 password=grupo105');
-    insert_statement = 'INSERT INTO usuarios(nombre_usuario, contrasena, tipo) VALUES('''||p_nombre||''', ''productora'');';
+    insert_statement = 'INSERT INTO usuarios(nombre_usuario, contrasena, tipo) VALUES('''||p_nombre||''', ''12345'',''productora'');';
     res := dblink_exec('db2', insert_statement, true); --lower(replace('''||p_nombre||''', '' '', ''_'')), md5(random()::text)
     RAISE INFO '%', res;
     perform dblink_disconnect('db2');
