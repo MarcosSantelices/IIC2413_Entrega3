131e300386f24622d9dacccd9e2aa1203e35412a
emiliochandia <emilio.chandia@uc.cl>
2022-12-02 22:15:06 -0300

    dblink2

--- stored_procedures/crear_usuario_productora.sql
+++ stored_procedures/crear_usuario_productora.sql
@@ -6,7 +6,7 @@ RETURNS BOOLEAN AS $$
 
 BEGIN
     PERFORM dblink_connect('db2', 'dbname=grupo105e3 user=grupo100 password=grupo100');
-    insert_statement = insert into usuarios(nombre, contrasena, tipo) values(lower(replace(p_nombre, ' ', '_')), md5(random()::text), 'productora');
+    insert_statement = 'insert into usuarios(nombre, contrasena, tipo) values(lower(replace(p_nombre, ' ', '_')), md5(random()::text), 'productora');';
     res := dblink_exec('db2', insert_statement, true);
     RAISE INFO '%', res;
     perform dblink_disconnect('db2');
